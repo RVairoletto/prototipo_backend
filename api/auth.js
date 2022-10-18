1ef5c656b5e0fc50699b22134971947c91037ea5
Ettore <ettsegura@gmail.com>
2022-10-18 12:27:31 -0300

    fix

--- api/auth.js
+++ api/auth.js
@@ -18,7 +18,7 @@ module.exports = app =>{
         const isMatch = bcrypt.compareSync(req.body.password, user.password)
         if(!isMatch) return res.status(401).send({"error":"Email/Senha incorretos"})
 
-        if(req.body.disabled){
+        if(req.body.disabled== true){
         return res.status(400).send({"error":"Usuário desabilitado"})
         }
 
