0a3d82721e181c2c9be85817748d8cd0389531e6
Ettore <ettsegura@gmail.com>
2022-10-20 09:49:57 -0300

    fixes

--- api/user.js
+++ api/user.js
@@ -77,8 +77,8 @@ module.exports =  app =>{
         const user = {...req.body}
 
         app.db('users')
-                .update(user)
                 .where({id:user.id})
+                .update(user)
                 .then(_=> res.status(204).send())
                 .catch(err=> res.status(500).send({err:"Não foi possivel editar o usuário"}))
 
